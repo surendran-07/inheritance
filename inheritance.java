276a366e2a3cb48e5a6d982f184960a07c8daf58
surendran <162984948+surendran-07@users.noreply.github.com>
2024-08-14 11:25:30 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,18 @@
+package surendran01;
+class employee{
+	float salary=40000;
+}
+class program extends employee{
+	int bonus=10000;
+	
+}
+public class prg5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+program p =new program();
+System.out.println("the salary is "+p.salary);
+System.out.println("the bonus is "+p.bonus);
+	}
+
+}
